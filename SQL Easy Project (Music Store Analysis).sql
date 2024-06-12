cf17b69dfb654f178f1496b5d832138aeea7b2f9
Ravi Singh Tomar <ujjain.ravitomar566@gmail.com>
2024-06-12 16:55:38 +0530

    Add files via upload

---
+++ SQL Easy Project (Music Store Analysis).sql
@@ -0,0 +1,38 @@
+--Easy-- 
+--Q1--Who is the senior most employee based on the job title?
+SELECT levels, employee_id, first_name, last_name, title
+FROM employee
+ORDER BY levels DESC
+LIMIT 1;
+
+--Q2--Which country has the most invoices?
+SELECT billing_country, COUNT(total) AS total_invoice
+FROM invoice
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
+
+--Q3--What are the top 3 values of total invoices?
+SELECT total, billing_country, billing_city
+FROM invoice
+ORDER BY total DESC
+LIMIT 3;
+
+--Q4--Which city has the best customers? We would like to throw a promotional music festival in the
+--city we made the most money. Write a query that returns one city that has the highest SUM of 
+--invoice total. Return both the city name and the sum of all invoices total?
+SELECT billing_city, SUM(total) AS invoice_total 
+FROM invoice
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
+
+--Q5--Who is the best customer? The customer who has spent the most money will be declared as the 
+--best customer. Write a query that returns the person who has spent the most money.
+SELECT c.customer_id, c.first_name, c.last_name, SUM(i.total) AS total_spent
+FROM customer AS c
+INNER JOIN invoice AS i ON c.customer_id = i.customer_id
+GROUP BY 1 
+ORDER BY 4 DESC
+LIMIT 1;
+
---
+++ SQL Moderate Project (Music Store Analysis).sql
@@ -0,0 +1,36 @@
+--Moderate--
+--Write a query to return the email, first name, last name and genre of all rock music listeners.
+--Return the list ordered alphabetically by email starting with A.
+SELECT customer.customer_id, customer.email, customer.first_name, customer.last_name, 
+	genre.name AS genre_name
+FROM customer
+INNER JOIN invoice ON invoice.customer_id = customer.customer_id
+INNER JOIN invoice_line AS il ON il.invoice_id = invoice.invoice_id
+INNER JOIN track ON track.track_id = il.track_id
+INNER JOIN genre ON genre.genre_id = track.genre_id
+WHERE genre.name LIKE 'Rock'
+GROUP BY 1,5
+ORDER BY 2 ASC;
+
+--Q2--Let's invite the artists who have written the most rock music in our dataset. Write a query 
+--that returns the artist name and total track count of the top 10 rock bands.
+SELECT artist.artist_id, artist.name, COUNT(track.track_id) AS track_count, genre.name AS genre
+FROM artist
+INNER JOIN album ON album.artist_id = artist.artist_id
+INNER JOIN track ON track.album_id = album.album_id
+INNER JOIN genre ON genre.genre_id = track.genre_id
+WHERE genre.name LIKE 'Rock'
+GROUP BY 1,4
+ORDER BY 3 DESC
+LIMIT 10;
+
+--Q3--Return all the track names that have a song length longer than the average song length.
+--Return the name and milliseconds for each track. Order by the song length with the longest songs 
+--listed first.
+SELECT track_id, name, milliseconds 
+FROM track
+WHERE milliseconds > (SELECT AVG(milliseconds) 
+                      FROM track)
+ORDER BY milliseconds DESC;
+
+
